1a33aebbbbed34dd23d2c7eb5aff44d535b795b9
Dmitriy <dmitriykolesnikov7@gmail.com>
2023-03-07 09:40:00 +0500

    fail CI

--- src/test/java/postmanEchoTest.java
+++ src/test/java/postmanEchoTest.java
@@ -14,7 +14,7 @@ public class postmanEchoTest {
                 .post("/post")
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("Hello World"));
+                .body("data", equalTo("Hello"));
     }
 
 }
